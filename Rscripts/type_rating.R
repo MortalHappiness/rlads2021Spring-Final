28630bc919d0308776963c5649ee7700fdf6adbb
dl8sd11 <tmd910607@gmail.com>
2021-06-07 22:27:15 +0800

    inspect rating vs type

---
+++ Rscripts/type_rating.R
@@ -0,0 +1,22 @@
+load("./Rdata/place.Rda") # place_df
+
+#  library(showtext)
+#  showtext_auto()
+
+library(tidyverse)
+library(ggplot2)
+
+place_df_without_na <- place_df %>%
+  filter(!is.na(rating))
+
+first_type_rating <- place_df_without_na %>%
+  group_by(first_type) %>%
+  summarize(n=n(), avg=mean(rating), std=sd(rating)) %>%
+  arrange(desc(n))
+
+
+#  x11()
+place_df_without_na %>%
+#  filter(first_type %in% c('restaurant', 'cafe', 'bakery', 'health')) %>%
+  ggplot(aes(x=as.factor(first_type), y=rating)) +
+  geom_boxplot()
