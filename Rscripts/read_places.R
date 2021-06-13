ba662fee0d9a974d131faef9040c36fa5d178886
MortalHappiness <b07901069@ntu.edu.tw>
2021-06-13 23:53:12 +0800

    Read place and filter places without reviews

--- Rscripts/read_places.R
+++ Rscripts/read_places.R
@@ -1,6 +1,7 @@
 library(rjson)
 library(tidyverse)
 
+place_ids <- tools::file_path_sans_ext(list.files("../data/reviews"))
 place_list <- fromJSON(file = "../data/places.json")
 len <- length(place_list)
 
@@ -21,6 +22,6 @@ place_df <- tibble(
   types = types,
   rating = rating,
   user_ratings_total = user_ratings_total,
-)
+) %>% filter(id %in% place_ids)
 
 save(place_df, file = "../Rdata/place.Rda")
