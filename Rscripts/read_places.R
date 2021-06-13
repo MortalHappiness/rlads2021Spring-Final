58246d04a856e0f9b63efcc2dc57dcfd996a8632
MortalHappiness <b07901069@ntu.edu.tw>
2021-06-13 22:36:50 +0800

    Update places.Rda

---
+++ Rscripts/read_places.R
@@ -0,0 +1,26 @@
+library(rjson)
+library(tidyverse)
+
+place_list <- fromJSON(file = "../data/places.json")
+len <- length(place_list)
+
+id <- names(place_list)
+name <- sapply(place_list, function(x) x$name)
+types <- lapply(place_list, function(x) x$types)
+rating <- lapply(place_list, function(x) x$rating)
+user_ratings_total <- sapply(place_list, function(x) x$user_ratings_total)
+
+rating[sapply(rating, is.null)] <- NA
+rating <- unlist(rating)
+user_ratings_total[sapply(user_ratings_total, is.null)] <- NA
+user_ratings_total <- unlist(user_ratings_total)
+
+place_df <- tibble(
+  id = id,
+  name = name,
+  types = types,
+  rating = rating,
+  user_ratings_total = user_ratings_total,
+)
+
+save(place_df, file = "../Rdata/place.Rda")
